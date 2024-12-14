b40aef5aedd17cb9ee7b203cc98d8627855aaba5
Teddy Zhai <teddy.zhai@gmx.com>
2024-12-14 13:58:42 +0100

    Right tree view

--- tree.cpp
+++ tree.cpp
@@ -12,19 +12,15 @@
  * };
  */
 
- #include <iostream>
+ #include <cstddef>
+#include <functional>
+#include <iostream>
+#include <map>
 #include <queue>
+#include <stack>
 #include <vector>
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
-
-using namespace std;
+#include "treeHelper.hpp"
+#include "vechelper.hpp"
 
 void BFS(TreeNode* root) {
 
@@ -119,6 +115,71 @@ void preOrderDFS(TreeNode* node) {
     preOrderDFS(node->right);
 }
 
+// post-order: root, right, left?
+
+
+vector<int> rightSideView(TreeNode* root) {
+    vector<int> res;
+
+    // DS: level -> stack
+    map<int, stack<TreeNode*>> level_nodes;
+
+    function<void(TreeNode*, int)> pre_order = [&](TreeNode *n, int level){
+        if (n == nullptr)
+        {
+            return ;
+        }
+        level_nodes[level].push(n);
+
+        pre_order(n->left, level+1);
+        pre_order(n->right, level+1);
+    };
+
+    pre_order(root, 0);
+
+    for(auto &i : level_nodes)
+    {
+        if (!(i.second.empty()))
+        {
+            auto *n = i.second.top();
+            if (n != nullptr)
+            {
+                res.push_back(n->val);
+            }
+            else
+            {
+                res.push_back(0);
+            }
+        }
+        else
+        {
+            res.push_back(0);
+        }
+    }
+
+    return res;
+
+}
+
+TreeNode* construct_rightView_tree(/* TreeNode *root */)
+{
+    // level 0:
+    TreeNode *root = new TreeNode(1);
+
+    // level 1:
+    TreeNode *l1 = new TreeNode(2);
+    TreeNode *r1 = new TreeNode(3);
+    root->left = l1;
+    root->right = r1;
+
+    // level 2:
+    TreeNode *l2_1 = new TreeNode(5);
+    TreeNode *l2_2 = new TreeNode(4);
+    l1->right = l2_1;
+    r1->right = l2_2;
+
+    return root;
+}
 
 int main(int argc, const char** argv) {
     TreeNode root(3);
@@ -146,6 +207,11 @@ int main(int argc, const char** argv) {
     }
 
     std::cout << "node num: " << getNrNodes(&root) << std::endl;
+
+    TreeNode *root2 = construct_rightView_tree(/* root2 */);
+    auto res2 = rightSideView(root2);
+    printVec(res2, "right side view");
+
 
 
     return 0;
